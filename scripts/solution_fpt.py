9edfc8e7fcda4da01db74a471940ed16bc139321
GerardArgany <gerardargany@gmail.com>
2025-12-27 20:30:03 +0100

    Comparison between theoretical and simulated FPT distribution

--- scripts/solution_fpt.py
+++ scripts/solution_fpt.py
@@ -3,30 +3,49 @@ import numpy as np
 
 
 def B(n, m0):
-	C = eval_gegenbauer(n, 1.5, m0)
-	return( (2*n+3)/((n+1)*(n+2))*C)
-	
-
-def la(n,N):
-	return(1/(N)*n*(n+1))
+    C = eval_gegenbauer(n, 1.5, m0)
+    return ((2*n+3)/((n+1)*(n+2))*C)
 
 
-def dist_laplace(N, m0, r, s, M = 600):
-
-	s1 = np.sum( [ B(2*i, m0)*la(2*i+1, N)/(s+r+la(2*i+1, N)) for i in range(M)] )
-	s2 = np.sum( [ B(2*i, m0)/(s+r+la(2*i+1, N)) for i in range(M)] )
-
-	return( (1-m0**2)*s1/ ( 1-r*(1-m0**2)*s2 ) )
+def la(n, N):
+    return (1/(N)*n*(n+1))
 
 
-def sol_fpt(N, m0, r, s_vals, M = 600):
-	return([dist_laplace(N, m0, r, s, M) for s in s_vals])
+def dist_laplace(N, m0, r, s, M=1000):
+    r = r/N
+
+    i = np.arange(M)
+    n_even = 2 * i
+    n_odd = n_even + 1
+
+    b_vals = B(n_even, m0)
+    la_vals = la(n_odd, N)
+    
+    denom = s + r + la_vals
+
+    s1 = np.sum(b_vals * la_vals / denom)
+    s2 = np.sum(b_vals * (s + la_vals) / denom)
+
+    return s1 / s2
 
 
-def mean_fpt(N, m0, r, M = 600):
-	r = r/N
-	s = np.sum( [ B(2*i, m0)/(r+la(2*i+1, N)) for i in range(M)] )
-	return( (1-m0**2)*s / ( 1-r*(1-m0**2)*s ) )
+def sol_fpt(N, m0, r, s_vals, M=1000):
+    return [dist_laplace(N, m0, r, s, M) for s in s_vals]
 
 
+def mean_fpt(N, m0, r, M=1000):
+    r = r/N
 
+    i = np.arange(M)
+    n_even = 2 * i
+    n_odd = n_even + 1
+    
+    b_vals = B(n_even, m0)
+    la_vals = la(n_odd, N)
+
+    denom = r + la_vals
+    
+    s = np.sum(b_vals / denom)
+    s2 = np.sum(b_vals * la_vals / denom)
+
+    return s / s2
