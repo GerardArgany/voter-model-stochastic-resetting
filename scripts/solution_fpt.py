5a5753ab21716ac0c42d27a93ab1c38c0b622afb
GerardArgany <gerardargany@gmail.com>
2026-03-09 18:10:53 +0100

    More structure changes

--- scripts/solution_fpt.py
+++ scripts/solution_fpt.py
@@ -1,18 +1,57 @@
-from scipy.special import eval_gegenbauer
+"""
+Analytical solution for the first passage time (FPT) distribution of the voter
+model with stochastic resetting.
+
+This module computes the Laplace transform of the first passage time distribution,
+the mean FPT, and the variance of the FPT for the all-to-all voter model under
+delta resetting, using an expansion in Gegenbauer polynomials.
+"""
+
 import numpy as np
+from scipy.special import eval_gegenbauer
 
 
-def B(n, m0):
+def B(n: int, m0: float) -> float:
+    """Compute the Gegenbauer expansion coefficient for mode n at reset position m0.
+
+    Args:
+        n: Expansion mode index (non-negative integer).
+        m0: Reset magnetisation in [-1, 1].
+
+    Returns:
+        Expansion coefficient for mode n.
+    """
     C = eval_gegenbauer(n, 1.5, m0)
-    return ((2*n+3)/((n+1)*(n+2))*C)
+    return (2 * n + 3) / ((n + 1) * (n + 2)) * C
 
 
-def la(n, N):
-    return (1/(N)*n*(n+1))
+def la(n: int, N: int) -> float:
+    """Compute the relaxation rate for mode n in a system of size N.
+
+    Args:
+        n: Mode index.
+        N: System size (number of agents).
+
+    Returns:
+        Relaxation rate lambda_n = n(n+1)/N.
+    """
+    return n * (n + 1) / N
 
 
-def dist_laplace(N, m0, r, s, M=1000):
-    r = r/N
+def dist_laplace(N: int, m0: float, r: float, s: float, M: int = 1000) -> float:
+    """Evaluate the Laplace transform of the FPT distribution at frequency s.
+
+    Args:
+        N: System size (number of agents).
+        m0: Initial (reset) magnetisation in [-1, 1].
+        r: Resetting rate (will be rescaled by 1/N internally).
+        s: Laplace variable (frequency).
+        M: Number of Gegenbauer modes to sum. Default is 1000.
+
+    Returns:
+        Value of the Laplace-transformed FPT distribution at s.
+    """
+    r = r / N
 
     i = np.arange(M)
     n_even = 2 * i
@@ -20,7 +59,7 @@ def dist_laplace(N, m0, r, s, M=1000):
 
     b_vals = B(n_even, m0)
     la_vals = la(n_odd, N)
-    
+
     denom = s + r + la_vals
 
     s1 = np.sum(b_vals * la_vals / denom)
@@ -29,48 +68,90 @@ def dist_laplace(N, m0, r, s, M=1000):
     return s1 / s2
 
 
-def sol_fpt(N, m0, r, s_vals, M=1000):
+def sol_fpt(N: int, m0: float, r: float, s_vals: np.ndarray, M: int = 1000) -> list:
+    """Compute the Laplace transform of the FPT distribution at multiple frequencies.
+
+    Args:
+        N: System size (number of agents).
+        m0: Initial (reset) magnetisation in [-1, 1].
+        r: Resetting rate.
+        s_vals: Array of Laplace-space frequencies at which to evaluate.
+        M: Number of Gegenbauer modes to sum. Default is 1000.
+
+    Returns:
+        List of Laplace-transformed FPT values corresponding to each element of s_vals.
+    """
     return [dist_laplace(N, m0, r, s, M) for s in s_vals]
 
 
-def mean_fpt(N, m0, r, M=1000):
-    #There is a simplification using an identity
-    r = r/N
+def mean_fpt(N: int, m0: float, r: float, M: int = 1000) -> float:
+    """Compute the mean first passage time to consensus.
+
+    Uses an algebraic simplification based on a Gegenbauer identity to avoid
+    a numerical derivative of the Laplace transform.
+
+    Args:
+        N: System size (number of agents).
+        m0: Initial (reset) magnetisation in [-1, 1].
+        r: Resetting rate (will be rescaled by 1/N internally).
+        M: Number of Gegenbauer modes to sum. Default is 1000.
+
+    Returns:
+        Mean first passage time to consensus (m = ±1).
+    """
+    r = r / N
 
     i = np.arange(M)
     n_even = 2 * i
     n_odd = n_even + 1
-    
+
     b_vals = B(n_even, m0)
     la_vals = la(n_odd, N)
 
     denom = r + la_vals
-    
+
     s = np.sum(b_vals / denom)
     s2 = np.sum(b_vals * la_vals / denom)
 
     return s / s2
 
-def variance_fpt(N, m0, r, M=1000):
-    r = r/N
+
+def variance_fpt(N: int, m0: float, r: float, M: int = 1000) -> float:
+    """Compute the standard deviation of the first passage time to consensus.
+
+    Note: Despite the name ``variance_fpt``, this function returns the
+    **standard deviation** (square root of the variance), not the variance
+    itself. The name is kept for backward compatibility with the original
+    scripts.
+
+    Args:
+        N: System size (number of agents).
+        m0: Initial (reset) magnetisation in [-1, 1].
+        r: Resetting rate (will be rescaled by 1/N internally).
+        M: Number of Gegenbauer modes to sum. Default is 1000.
+
+    Returns:
+        Standard deviation of the first passage time.
+    """
+    r = r / N
 
     i = np.arange(M)
     n_even = 2 * i
     n_odd = n_even + 1
-    
+
     b_vals = B(n_even, m0)
     la_vals = la(n_odd, N)
-    
+
     denom = r + la_vals
     s = np.sum(b_vals / denom)
     s2 = np.sum(b_vals * la_vals / denom)
-    s3 = np.sum(b_vals / (denom**2))
+    s3 = np.sum(b_vals / (denom ** 2))
 
     mean = s / s2
-    
-    prefactor = 1 - m0**2  
-    real_denom_sq = (prefactor * s2)**2    
+
+    prefactor = 1 - m0 ** 2
+    real_denom_sq = (prefactor * s2) ** 2
     real_num = 2 * prefactor * s3
     second_moment = real_num / real_denom_sq
-    
-    return np.sqrt(second_moment - mean**2)
+
+    return np.sqrt(second_moment - mean ** 2)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,21 @@
+"""
+Utility functions for the voter model with stochastic resetting.
+"""
+
 import numpy as np
 
-def laplace_from_samples(T, s):
+
+def laplace_from_samples(T: np.ndarray, s: float) -> float:
+    """Estimate the Laplace transform of a distribution from samples.
+
+    Computes the empirical average of exp(-s * T) over the provided samples,
+    which is an unbiased estimator of the Laplace transform E[e^{-sT}].
+
+    Args:
+        T: Array of samples drawn from the distribution of interest.
+        s: Laplace variable (frequency) at which to evaluate.
+
+    Returns:
+        Empirical estimate of the Laplace transform at s.
+    """
     return np.mean(np.exp(-s * T))
